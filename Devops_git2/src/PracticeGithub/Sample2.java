ca70a2f2fa901423d153e7d54c3fc0556d2819bf
keerthinagareddy10 <ln555r@att.com>
2017-04-24 20:41:48 +0530

    update to version2

--- Devops_git2/src/PracticeGithub/Sample2.java
+++ Devops_git2/src/PracticeGithub/Sample2.java
@@ -9,6 +9,8 @@ public class Sample2 {
 		// TODO Auto-generated method stub
 		System.out.println("Intial version 1");
 		System.out.println("intail Sample code");
+		System.out.println("Update to version 2");
+		System.out.println("Sample code changed");
 	}
 
 }
