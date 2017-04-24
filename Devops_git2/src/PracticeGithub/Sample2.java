174858666b475194864354ece2ab53dc012672a6
keerthinagareddy10 <ln555r@att.com>
2017-04-24 20:26:05 +0530

    initial version code

---
+++ Devops_git2/src/PracticeGithub/Sample2.java
@@ -0,0 +1,14 @@
+package PracticeGithub;
+
+public class Sample2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Intial version 1");
+		System.out.println("intail Sample code");
+	}
+
+}
